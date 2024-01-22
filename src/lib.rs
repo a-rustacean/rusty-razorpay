96297328c9df98773d2c356a3c92e1ceb51d5ea0
Dilshad <a-rustacean@outlook.com>
2024-01-22 23:09:53 +0530

    fix(lib): link modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 mod api;
 mod razorpay;
 
+pub mod account;
 pub mod addon;
+pub mod card;
 pub mod common;
+pub mod customer;
+pub mod dispute;
+pub mod document;
 pub mod error;
+pub mod item;
+pub mod offer;
 pub mod order;
 pub mod payment;
 pub mod plan;
+pub mod refund;
+pub mod settlement;
 pub mod subscription;
 pub mod util;
+pub mod webhook;
 
 pub use razorpay::{Razorpay, RazorpayOptions, VERSION};
